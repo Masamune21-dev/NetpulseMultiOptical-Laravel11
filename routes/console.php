0338d4bb8b00b431f0baf8d4fc0b6f961ce6241c
root <root@KusumaVisionNMS.1.1.1.1>
2026-09-07 20:59:12 +0000

    Revert "perf(polling): fast probe dead snmp devices to drop cycle time from 67s to 5s, restoring 1-minute cadence"
    This reverts commit c7153576f62e88289eb6445d761ce4228a811994.

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('poll:interfaces --timeout=30')->everyMinute()->withoutOverlapping(10);
+Schedule::command('poll:interfaces')->everyMinute()->withoutOverlapping();
 
 // Downsample optical stats into hourly/daily rollups so long-range redaman
 // history is preserved cheaply. Runs a few minutes past the hour to capture
